b58361c3c90089a8ebc57832bf0cb1c5c2f43e27
shubh586 <shubhamsolanke9326@gmail.com>
2025-06-24 20:39:16 +0530

    addeded functions

---
+++ YoutubeQASummary/pipeline/embedding.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from dotenv import load_dotenv
+import os
+
+
+def getEmbeddinModel():
+    embedding_model= HuggingFaceEndpointEmbeddings(
+    model="Snowflake/snowflake-arctic-embed-l-v2.0",
+    task="feature-extraction",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACE_API_KEY")
+    
+    )
+    return embedding_model
+
